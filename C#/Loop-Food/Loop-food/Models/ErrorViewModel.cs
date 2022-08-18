13ced7fea8a90ff8755d5d7ca4b1d7102c9a187e
Oskar Bielak <oskar.bielak@mixbox.pl>
2022-08-18 20:21:22 +0200

    C#

---
+++ C#/Loop-Food/Loop-food/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Loop_food.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

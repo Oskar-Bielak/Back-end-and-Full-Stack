d5e2bbd649fa21b2f01c6b3cb24028d66cf049c4
Oskar Bielak <56562887+Oskar-Bielak@users.noreply.github.com>
2022-09-03 13:07:10 +0200

    Delete PHP/Aplikacje/Loop-Food directory

--- PHP/Aplikacje/Loop-Food/Admin/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-   
-    session_unset();
-
-    header('Location: /Loop-Food/Aplikacja/Admin/admin')
-    ?>
--- PHP/Aplikacje/Loop-Food/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-   
-    session_unset();
-
-    header('Location: /Loop-Food/Aplikacja/aplikacja-jedzienie')
-    ?>

19b3987ba654648ef5da7c1fe70daeb2b557ac3d
Oskar Bielak <oskar.bielak@mixbox.pl>
2022-09-06 20:34:45 +0200

    Prosty czat okienkowy

---
+++ C#/WP, M, I/Aplikacja Okienkowa Klient- Serwer/ChatClient/ChatClient/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ChatClient
+{
+    public partial class Form1 : Form
+    {
+        
+        TcpClient _client;
+
+        
+        byte[] _buffer = new byte[4096];
+
+        public Form1()
+        {
+            InitializeComponent();
+            _client = new TcpClient();
+        }
+
+        protected override void OnShown(EventArgs e)
+        {
+            base.OnShown(e);
+
+            
+            _client.Connect("127.0.0.1", 54000);
+
+            
+            _client.GetStream().BeginRead(_buffer,
+                                            0,
+                                            _buffer.Length,
+                                            Server_MessageReceived,
+                                            null);
+        }
+        
+        static byte[] EncryptStringToBytes_Aes(string plainText, byte[] Key, byte[] IV)
+            {
+                // Check arguments.
+                if (plainText == null || plainText.Length <= 0)
+                    throw new ArgumentNullException("plainText");
+                if (Key == null || Key.Length <= 0)
+                    throw new ArgumentNullException("Key");
+                if (IV == null || IV.Length <= 0)
+                    throw new ArgumentNullException("IV");
+                byte[] encrypted;
+
+                // Create an Aes object
+                // with the specified key and IV.
+                using (Aes aesAlg = Aes.Create())
+                {
+                    aesAlg.Key = Key;
+                    aesAlg.IV = IV;
+
+                    // Create an encryptor to perform the stream transform.
+                    ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                    // Create the streams used for encryption.
+                    using (MemoryStream msEncrypt = new MemoryStream())
+                    {
+                        using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                        {
+                            using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                            {
+                                //Write all data to the stream.
+                                swEncrypt.Write(plainText);
+                            }
+                            encrypted = msEncrypt.ToArray();
+                        }
+                    }
+                }
+
+                // Return the encrypted bytes from the memory stream.
+                return encrypted;
+        }
+
+        static string DecryptStringFromBytes_Aes(byte[] cipherText, byte[] Key, byte[] IV)
+        {
+            // Check arguments.
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+
+            // Declare the string used to hold
+            // the decrypted text.
+            string plaintext = null;
+
+            // Create an Aes object
+            // with the specified key and IV.
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+
+                // Create a decryptor to perform the stream transform.
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for decryption.
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+
+                            // Read the decrypted bytes from the decrypting stream
+                            // and place them in a string.
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+
+            return plaintext;
+        }
+       
+        private void Server_MessageReceived(IAsyncResult ar)
+        {
+            if (ar.IsCompleted)
+            {
+                
+                var bytesIn = _client.GetStream().EndRead(ar);
+                if (bytesIn > 0)
+                {
+                    
+                    var tmp = new byte[bytesIn];
+                    Array.Copy(_buffer, 0, tmp, 0, bytesIn);
+                    var str = Encoding.ASCII.GetString(tmp);
+
+                    
+                    BeginInvoke((Action)(() =>
+                    {
+                        listBox1.Items.Add(str);
+                        listBox1.SelectedIndex = listBox1.Items.Count - 1;
+                    }));
+                }
+
+                
+                Array.Clear(_buffer, 0, _buffer.Length);
+                _client.GetStream().BeginRead(_buffer,
+                                                0,
+                                                _buffer.Length,
+                                                Server_MessageReceived,
+                                                null);
+            }
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+{
+             using (Aes myAes = Aes.Create())
+            {
+                byte[] encrypted = EncryptStringToBytes_Aes(textBox1.Text, myAes.Key, myAes.IV);
+
+
+                string roundtrip = DecryptStringFromBytes_Aes(encrypted, myAes.Key, myAes.IV);
+
+                var msg = Encoding.ASCII.GetBytes(textBox1.Text);
+        
+
+                _client.GetStream().Write(msg, 0, msg.Length);
+                textBox1.Text = "";
+                textBox1.Focus();
+               
+
+                
+            }
+            
+
+
+           
+        }
+    }
+}
---
+++ C#/WP, M, I/Aplikacja Okienkowa Klient- Serwer/readme.md
@@ -0,0 +1,15 @@
+PL:
+
+Prosty komunikartor okienkowy:
+
+-Server napisany w c++ odpary na socketach, kazda osoba dostaje kolejna liczbe, moze działac na raz wiele okienek.
+
+-Klient napisany w C# użycie WinForms, oraz uzycie AES do szyfrowania i odszyfrowania kazdej wiadomosci pomiedzy uzyrtkownikami.
+
+EN:
+
+Simple window messenger:
+
+-Server written in c ++ sockets-proof, each person gets another number, can run multiple windows at once.
+
+-C # client written in C # using WinForms, and using AES to encrypt and decrypt every message between users.

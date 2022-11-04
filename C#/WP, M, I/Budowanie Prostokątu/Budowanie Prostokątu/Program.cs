1d78128eebb17d213e3f824d7d48d84a78405d10
Oskar Bielak <oskar.bielak@mixbox.pl>
2022-11-04 08:29:46 +0100

    ECC Szyfrowanie

---
+++ C#/WP, M, I/Budowanie Prostokątu/Budowanie Prostokątu/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Budowanie_Prostokątu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj szerokość X: ");
+            string xs = Console.ReadLine();
+            Console.WriteLine("Podaj wysokość Y: ");
+            string ys = Console.ReadLine();
+            
+            int x = int.Parse(xs);
+            int y = int.Parse(ys);
+
+
+
+
+
+        }
+    }
+}
---
+++ C#/WP, M, I/Szyfrowanie Plików Algorytmem ECC/Szyfrowanie Plików Algorytmem ECC/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Szyfrowanie_Plików_Algorytmem_ECC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        fileContent = reader.ReadToEnd();
+                    }
+                }
+            }
+
+            MessageBox.Show(fileContent, "File Content at path: " + filePath, MessageBoxButtons.OK);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
